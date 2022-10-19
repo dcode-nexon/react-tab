fe96875a8ccf8255d7c27f9ae34a9874ba4f3ab2
tubi55 <tubi55@nate.com>
2022-10-19 13:19:16 +0900

    기본 탭 데이터 호출

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './scss/style.scss';
 import 'axios';
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+	const [Data, setData] = useState([]);
+
+	useEffect(() => {
+		axios.get(process.env.PUBLIC_URL + '/DB/data.json').then((json) => {
+			//console.log(json.data.data);
+			setData(json.data.data);
+		});
+	}, []);
+
+	useEffect(() => {
+		console.log(Data);
+	}, [Data]);
 	return <></>;
 }
 
