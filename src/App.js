70013963fd8bb749867b55a1d950935c0ae46070
tubi55 <tubi55@nate.com>
2022-10-19 13:22:37 +0900

    async await로 비동기 데이터호출 개선

--- src/App.js
+++ src/App.js
@@ -6,11 +6,9 @@ import axios from 'axios';
 function App() {
 	const [Data, setData] = useState([]);
 
-	useEffect(() => {
-		axios.get(process.env.PUBLIC_URL + '/DB/data.json').then((json) => {
-			//console.log(json.data.data);
-			setData(json.data.data);
-		});
+	useEffect(async () => {
+		const result = await axios.get(process.env.PUBLIC_URL + '/DB/data.json');
+		setData(result.data.data);
 	}, []);
 
 	useEffect(() => {
